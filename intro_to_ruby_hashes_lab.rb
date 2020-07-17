204aee9dc9f8a87b57efaaf938a427c37c3133de
Shelley Brook <sbrook13@gmail.com>
2020-07-17 17:02:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,11 @@ def monopoly_with_second_tier
   monopoly_hash = {
     :railroads => {}
   }
-  monopoly_hash[:railroads][:pieces] = 4
-  monopoly_hash[:railroads][:rent_in_dollars] = {}
-  monopoly_hash[:railroads][:names] = {}
-
+  monopoly_hash[:railroads] = {
+    :pieces => 4,
+    :rent_in_dollars = {},
+    :names] = {}
+  }
 end
 
 def monopoly_with_third_tier
